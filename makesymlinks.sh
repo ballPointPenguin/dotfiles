efaf5cfcc8ec3956b891c27a7f2e10c298979419
ballPointPenguin <rosas.ben@gmail.com>
2012-09-28 19:50:26 -0700

    script reflects actual dotfiles

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
-files="Xresources bash_login bash_profile bashrc emacs gitconfig profile tmux.conf zlogin zshrc" # list of files/folders to symlink in homedir
+files="bash_login bash_profile bashrc emacs gitconfig profile tmux.conf vimrc.after Xresources xsession zlogin zshrc" # list of files/folders to symlink in homedir
 
 ##########
 
